4a523c420386fa71feb9e80d87bc52f0e83d8b0d
Matheus Canovas <matheuscanovas9@gmail.com>
2023-04-23 13:57:20 -0300

    Creating project

---
+++ src/main/java/com/github/omtcnvs/api/service/registry/sample/ApiServiceRegistrySampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.omtcnvs.api.service.registry.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServiceRegistrySampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiServiceRegistrySampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/omtcnvs/api/service/registry/sample/ApiServiceRegistrySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.omtcnvs.api.service.registry.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServiceRegistrySampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
